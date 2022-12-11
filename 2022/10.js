e964979a849df7f08193be22a6764bda762b11fb
Connor Hewitt <connor.hewitt@gmail.com>
2022-12-11 15:41:38 -0600

    Day 10 part 2

--- 2022/10.js
+++ 2022/10.js
@@ -1,6 +1,6 @@
 const _ = require("underscore")
 
-function part1() {
+function calculateValues() {
     let values = [undefined, 1];
     
     let commands = input.split('\n');
@@ -14,12 +14,35 @@ function part1() {
             throw new Error("bad command");
         }
     }
+    
+    return values;
+}
+
+function part1() {
+    let values = calculateValues();
 
     return 20 * values[20] + 60 * values[60] + 100 * values[100] + 140 * values[140] + 180 * values[180] + 220 * values[220];
 }
 
 function part2() {
+    let values = calculateValues();
 
+    let ret = [];
+    for(let i = 0; i < 6; ++i) {
+        ret[i] = new Array(40);
+        ret[i].fill(" ");
+    }
+
+    for(let y = 0; y < 6; ++y) {
+        for(let x = 0; x < 40; ++x) {
+            let index = y * 40 + x + 1;
+            if([x-1, x, x+1].includes(values[index])) {
+                ret[y][x] = '#';
+            }
+        }
+    }
+
+    return ret.map(a=>a.join('')).join('\n');
 }
 
 const input = `noop
