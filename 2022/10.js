66cf93a1fc7b25bca3eebb996e73243379c2dccb
Connor Hewitt <connor.hewitt@gmail.com>
2022-12-11 14:53:40 -0600

    Day 10 part 1

---
+++ 2022/10.js
@@ -0,0 +1,173 @@
+const _ = require("underscore")
+
+function part1() {
+    let values = [undefined, 1];
+    
+    let commands = input.split('\n');
+    for(let i = 0; i < commands.length; ++i) {
+        if(commands[i][0] == "n") {
+            values.push(_.last(values));
+        } else if(commands[i][0] == "a") {
+            values.push(_.last(values));
+            values.push(_.last(values) + parseInt(commands[i].split(' ')[1]));
+        } else {
+            throw new Error("bad command");
+        }
+    }
+
+    return 20 * values[20] + 60 * values[60] + 100 * values[100] + 140 * values[140] + 180 * values[180] + 220 * values[220];
+}
+
+function part2() {
+
+}
+
+const input = `noop
+addx 5
+addx -2
+noop
+noop
+addx 7
+addx 15
+addx -14
+addx 2
+addx 7
+noop
+addx -2
+noop
+addx 3
+addx 4
+noop
+noop
+addx 5
+noop
+noop
+addx 1
+addx 2
+addx 5
+addx -40
+noop
+addx 5
+addx 2
+addx 15
+noop
+addx -10
+addx 3
+noop
+addx 2
+addx -15
+addx 20
+addx -2
+addx 2
+addx 5
+addx 3
+addx -2
+noop
+noop
+noop
+addx 5
+addx 2
+addx 5
+addx -38
+addx 3
+noop
+addx 2
+addx 5
+noop
+noop
+addx -2
+addx 5
+addx 2
+addx -2
+noop
+addx 7
+noop
+addx 10
+addx -5
+noop
+noop
+noop
+addx -15
+addx 22
+addx 3
+noop
+noop
+addx 2
+addx -37
+noop
+noop
+addx 13
+addx -10
+noop
+addx -5
+addx 10
+addx 5
+addx 2
+addx -6
+addx 11
+addx -2
+addx 2
+addx 5
+addx 3
+noop
+addx 3
+addx -2
+noop
+addx 6
+addx -22
+addx 23
+addx -38
+noop
+addx 7
+noop
+addx 5
+noop
+noop
+noop
+addx 9
+addx -8
+addx 2
+addx 7
+noop
+noop
+addx 2
+addx -4
+addx 5
+addx 5
+addx 2
+addx -26
+addx 31
+noop
+addx 3
+noop
+addx -40
+addx 7
+noop
+noop
+noop
+noop
+addx 2
+addx 4
+noop
+addx -1
+addx 5
+noop
+addx 1
+noop
+addx 2
+addx 5
+addx 2
+noop
+noop
+noop
+addx 5
+addx 1
+noop
+addx 4
+addx 3
+noop
+addx -24
+noop`;
+
+console.log(part1());
+console.log(part2());
